3021491c6c155504e42c42c8db0aea376c71d355
Matt B <jackdachef@gmail.com>
2011-06-02 22:16:11 +0200

    Added back smartassv2

--- setenv.sh
+++ setenv.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 export ARCH=arm 
-export TOOLCHAIN=/home/nushor/android-toolchain-eabi
+#export TOOLCHAIN=/usr
+#export TOOLCHAIN=/home/nushor/android-toolchain-eabi
 export TOOLCHAIN=/home/nushor/arm-2010q1
+#export TOOLPREFIX=arm-linux-gnueabi
 #export TOOLPREFIX=arm-eabi
 export TOOLPREFIX=arm-none-eabi
-#export PATH=$PATH:${TOOLCHAIN}/bin:/usr/java/jdk1.6.0_21/bin
+export PATH=$PATH:${TOOLCHAIN}/bin:/usr/java/jdk1.6.0_21/bin
 export CROSS_COMPILE=${TOOLCHAIN}/bin/${TOOLPREFIX}-
 export CC=${TOOLCHAIN}/bin/${TOOLPREFIX}-gcc
 export LD=${TOOLCHAIN}/bin/${TOOLPREFIX}-ld
@@ -15,10 +17,10 @@ export PATH=$PATH:${TOOLCHAIN}/
 export PATH=$PATH:${TOOLCHAIN}/lib
 #export CFLAGS="-mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp -O2 -fstack-protector -fstack-protector-all -fno-gcse -fprefetch-loop-arrays --param l2-cache-size=512 --param l1-cache-size=64 --param simultaneous-prefetches=6 --param prefetch-latency=400 --param l1-cache-line-size=64 -std=c99"
 export CFLAGS="-static -Os -fstack-protector -fstack-protector-all -std=c99"
-#export CLFAGS="-Wall -Wmissing-prototypes -Wstrict-prototypes -static -Os -fomit-frame-pointer"
+#export CLFAGS="-Wall -Wmissing-prototypes -Wstrict-prototypes -static -O2 -fomit-frame-pointer"
 export LDFLAGS="-static -Os"
-#export CPPFLAGS="-static -O2"
 export CPPFLAGS="-static -Os"
+#export CPPFLAGS="-static -Os"
 echo "ARM environment set"
 #cd stable
 #unzip -o prebuilt.zip
