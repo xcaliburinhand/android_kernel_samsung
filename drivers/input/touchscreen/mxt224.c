0c4559ab0722dc00bb871336a9ddcc700405a997
Pawit Pornkitprasan <p.pawit@gmail.com>
2011-12-17 15:35:18 +0700

    Update touchscreen driver from crepo

--- drivers/input/touchscreen/mxt224.c
+++ drivers/input/touchscreen/mxt224.c
@@ -62,7 +62,6 @@ struct mxt224_data {
 	struct input_dev *input_dev;
 	struct early_suspend early_suspend;
 	u32 finger_mask;
-	u32 touch_mask;
 	int gpio_read_done;
 	struct object_t *objects;
 	u8 objects_len;
@@ -303,8 +302,10 @@ err:
 
 static void report_input_data(struct mxt224_data *data)
 {
-	int i, iter = 0;
+	int i;
+	int num_fingers_down;
 
+	num_fingers_down = 0;
 	for (i = 0; i < data->num_fingers; i++) {
 		if (data->fingers[i].z == -1)
 			continue;
@@ -313,31 +314,19 @@ static void report_input_data(struct mxt224_data *data)
 					data->fingers[i].x);
 		input_report_abs(data->input_dev, ABS_MT_POSITION_Y,
 					data->fingers[i].y);
-		input_report_abs(data->input_dev, ABS_MT_TOUCH_MAJOR,
+		input_report_abs(data->input_dev, ABS_MT_PRESSURE,
 					data->fingers[i].z);
-		input_report_abs(data->input_dev, ABS_MT_WIDTH_MAJOR,
+		input_report_abs(data->input_dev, ABS_MT_TOUCH_MAJOR,
 					data->fingers[i].w);
 		input_report_abs(data->input_dev, ABS_MT_TRACKING_ID, i);
 		input_mt_sync(data->input_dev);
-
-		if (data->fingers[i].z == 0) {
-			data->fingers[i].z = -1;
-			data->touch_mask &= ~(1U << i);
-		}
-		iter++;
+		num_fingers_down++;
 	}
-
-	if (iter == 0)
-		input_mt_sync(data->input_dev);
-
-	input_sync(data->input_dev);
-	
-	if (iter && data->touch_mask == 0) {
-		input_mt_sync(data->input_dev);
-		input_sync(data->input_dev);
-	}
-	
 	data->finger_mask = 0;
+
+	if (num_fingers_down == 0)
+		input_mt_sync(data->input_dev);
+	input_sync(data->input_dev);
 }
 
 static irqreturn_t mxt224_irq_thread(int irq, void *ptr)
@@ -365,7 +354,7 @@ static irqreturn_t mxt224_irq_thread(int irq, void *ptr)
 			data->fingers[id].z = -1;
 			data->fingers[id].w = msg[5];
 			data->finger_mask |= 1U << id;
-			data->touch_mask &= ~(1U << id);
+			touch_state_val = 0;
 		} else if ((msg[1] & DETECT_MSG_MASK) && (msg[1] &
 				(PRESS_MSG_MASK | MOVE_MSG_MASK))) {
 			data->fingers[id].z = msg[6];
@@ -375,10 +364,10 @@ static irqreturn_t mxt224_irq_thread(int irq, void *ptr)
 			data->fingers[id].y = ((msg[3] << 4) |
 					(msg[4] & 0xF)) >> data->y_dropbits;
 			data->finger_mask |= 1U << id;
-			data->touch_mask |= 1U << id;
+			touch_state_val = 1;
 		} else if ((msg[1] & SUPPRESS_MSG_MASK) &&
 			   (data->fingers[id].z != -1)) {
-			data->fingers[id].z = 0;
+			data->fingers[id].z = -1;
 			data->fingers[id].w = msg[5];
 			data->finger_mask |= 1U << id;
 		} else {
@@ -404,16 +393,11 @@ static int mxt224_internal_suspend(struct mxt224_data *data)
 	if (ret)
 		return ret;
 
+	touch_state_val = 0;
 
-	for (i = 0; i < data->num_fingers; i++) {
-		if (data->fingers[i].z == -1)
-			continue;
+	for (i = 0; i < data->num_fingers; i++)
 		data->fingers[i].z = -1;
-	}
-	data->touch_mask = 0;
-	data->finger_mask = 0;
-	input_mt_sync(data->input_dev);
-	input_sync(data->input_dev);
+	report_input_data(data);
 
 	data->power_off();
 
@@ -519,9 +503,9 @@ static int __devinit mxt224_probe(struct i2c_client *client,
 			pdata->max_x, 0, 0);
 	input_set_abs_params(input_dev, ABS_MT_POSITION_Y, pdata->min_y,
 			pdata->max_y, 0, 0);
-	input_set_abs_params(input_dev, ABS_MT_TOUCH_MAJOR, pdata->min_z,
+	input_set_abs_params(input_dev, ABS_MT_PRESSURE, pdata->min_z,
 			pdata->max_z, 0, 0);
-	input_set_abs_params(input_dev, ABS_MT_WIDTH_MAJOR, pdata->min_w,
+	input_set_abs_params(input_dev, ABS_MT_TOUCH_MAJOR, pdata->min_w,
 			pdata->max_w, 0, 0);
 	input_set_abs_params(input_dev, ABS_MT_TRACKING_ID, 0,
 			data->num_fingers - 1, 0, 0);
