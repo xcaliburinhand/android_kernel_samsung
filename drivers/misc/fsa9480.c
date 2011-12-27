d67199f7ba1724c24b9529679589a160174b349f
jt1134 <jt1134@gmail.com>
2011-12-27 01:18:29 -0600

    readd support for dock audio

--- drivers/misc/fsa9480.c
+++ drivers/misc/fsa9480.c
@@ -311,6 +311,7 @@ static void fsa9480_detect_dev(struct fsa9480_usbsw *usbsw)
                                         "%s: err %d\n", __func__, ret);
 #endif
 #else
+
 			ret = i2c_smbus_write_byte_data(client,
 					FSA9480_REG_MANSW1, SW_DHOST);
 			if (ret < 0)
@@ -362,6 +363,7 @@ static void fsa9480_detect_dev(struct fsa9480_usbsw *usbsw)
                                 dev_err(&client->dev,
                                         "%s: err %d\n", __func__, ret);
 #endif
+
 		}
 	/* Detached */
 	} else {
@@ -411,6 +413,8 @@ static void fsa9480_detect_dev(struct fsa9480_usbsw *usbsw)
 		} else if (usbsw->dev2 & DEV_JIG_UART_ON) {
 			if (pdata->cardock_cb)
 				pdata->cardock_cb(FSA9480_DETACHED);
+			dock_status = 0;
+
 #if defined(CONFIG_MACH_ARIES)
                         ret = i2c_smbus_read_byte_data(client,
                                         FSA9480_REG_CTRL);
@@ -424,6 +428,7 @@ static void fsa9480_detect_dev(struct fsa9480_usbsw *usbsw)
                                 dev_err(&client->dev,
                                         "%s: err %d\n", __func__, ret);
 #endif
+
 		}
 	}
 
