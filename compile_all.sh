6e8808683f55925f84e0227fb6c85ed52f43d954
nushor <nushor11@gmail.com>
2012-01-04 14:29:48 -0600

    Modify scripts, move sensors.aries.so to proper directory

--- compile_all.sh
+++ compile_all.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 make clean;
 ./compile_captivate.sh; 
+./compile_captivate.sh BIGMEM ;
 make clean;
 ./compile_vibrant.sh; 
+./compile_vibrant.sh BIGMEM ;
 make clean;
 ./compile_galaxys.sh; 
+./compile_galaxys.sh BIGMEM ;
 make clean;
 ./compile_galaxysb.sh;
+./compile_galaxysb.sh BIGMEM ;
+
 ~/sync.sh;
